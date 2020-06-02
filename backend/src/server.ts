cdbf4643def8e02fc6f4095590bdbbef56055b70
alexandreerick <erickalexandrex@gmail.com>
2020-06-02 12:38:47 -0300

    configuracao database

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-// app.use(express.json());
-
-app.get('/users', (req, res) => {
-  res.json([
-    "Erick",
-    "Emily",
-  ]);
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
