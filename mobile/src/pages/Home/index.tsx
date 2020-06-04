5e27e4a3375677d3c3efc8008fbad500e429fa12
alexandreerick <erickalexandrex@gmail.com>
2020-06-04 14:07:44 -0300

    Navegaçao configurada

--- mobile/src/pages/Home/index.tsx
+++ mobile/src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Feather as Icon } from "@expo/vector-icons";
 import { View, ImageBackground, Image, Text, StyleSheet } from "react-native";
+import { RectButton } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 const Home = () => {
+  const navigation = useNavigation();
+
+  function handleNavigateToPoints() {
+    navigation.navigate("Points");
+  }
+
   return (
     <ImageBackground
       style={styles.container}
@@ -15,6 +24,17 @@ const Home = () => {
           Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.
         </Text>
       </View>
+
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleNavigateToPoints}>
+          <View style={styles.buttonIcon}>
+            <Text>
+              <Icon name="arrow-right" color="#FFF" size={24} />
+            </Text>
+          </View>
+          <Text style={styles.buttonText}>Entrar</Text>
+        </RectButton>
+      </View>
     </ImageBackground>
   );
 };
@@ -23,7 +43,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    backgroundColor: "#f0f0f5",
   },
 
   main: {
